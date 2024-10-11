6f97e34de72b388fdf15dd0ef2b1b9e19f92f273
Abdulwahid <abusabrina2158@gmail.com>
2024-10-11 11:05:29 +0300

    front end updated

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,19 @@
 from django.urls import path
 from . import views
 from django.conf import settings
+from django.contrib.auth import views as auth_views
 from django.conf.urls.static import static
+from django.urls import reverse_lazy
 
 urlpatterns = [
     path('', views.upload_file, name='upload_file'),
     path('download/<int:file_id>/', views.download_file, name='download_file'),
     path('files/', views.list_uploaded_files, name='list_uploaded_files'),
     path('view_encrypted/<int:file_id>/', views.view_encrypted_file, name='view_encrypted_file'),
+    path('login/', auth_views.LoginView.as_view(template_name='core/login.html', success_url=reverse_lazy('list_uploaded_files')), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('signup/', views.signup_view, name='signup'), 
+    path('profile/', views.profile, name='profile'),
 ]
 
 
--- core/views.py
+++ core/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, redirect
-from .forms import FileUpload
+from .forms import FileUpload, CustomPasswordChangeForm
 from .models import FileEncryption
 from django.conf import settings
 from django.http import HttpResponse,  HttpResponseForbidden
 import base64
+from django.contrib.auth import authenticate, login as auth_login, update_session_auth_hash
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, PasswordChangeForm
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 def upload_file(request):
     success_message = None
@@ -86,3 +90,45 @@ def view_encrypted_file(request, file_id):
 
     except FileEncryption.DoesNotExist:
         return HttpResponse('File not found', status=404)
+
+
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                auth_login(request, user)  
+                return redirect('home')  # Redirect home page
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'core/login.html', {'form': form})
+
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # Redirect to login after signup
+    else:
+        form = UserCreationForm()
+    return render(request, 'core/signup.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        password_form = CustomPasswordChangeForm(user=request.user, data=request.POST)
+        if password_form.is_valid():
+            user = password_form.save()
+            messages.success(request, "Your password has been successfully updated!")
+            return redirect('profile')  # Redirect to profile page after successful password change
+    else:
+        password_form = CustomPasswordChangeForm(user=request.user)
+
+    return render(request, 'core/profile.html', {'password_form': password_form})
